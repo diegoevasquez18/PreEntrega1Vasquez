560cc7d12b41b1fe20dac7a84c20154df5ebf05f
diegoevasquez18 <71709152+diegoevasquez18@users.noreply.github.com>
2022-10-24 17:34:48 -0300

    Work Hard

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,12 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA-y9-E4_L9_IcXVJWtIDYorvRE4RJ1Q3E",
-  authDomain: "backend-ecommerce-44cfa.firebaseapp.com",
-  projectId: "backend-ecommerce-44cfa",
-  storageBucket: "backend-ecommerce-44cfa.appspot.com",
-  messagingSenderId: "301119266754",
-  appId: "1:301119266754:web:6b1bd28d8ea82635b6005b"
+
+  apiKey: process.env.REACT_APP_appKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId 
+
 };
 
 // Initialize Firebase
